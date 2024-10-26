183c1b448daa42aa66d1112aeb22124092d82dbd
A Cottrill <a.rcottrill521@gmail.com>
2024-10-25 21:20:52 -0400

    feat: add year four challenge

---
+++ Year_Four/node/doWork.js
@@ -0,0 +1,24 @@
+/**
+ * @param {(string|number)[]} inbox
+ */
+function humanResourceMachine(inbox) {
+  let outbox = [];
+  let floor = [];
+  for (let i = 0; i < inbox.length; i++)
+  {
+    let value = inbox[i]; 
+     if (i % 2 == 0)
+    {
+      floor[0] = value; 
+    }
+    else 
+    {
+      outbox.push(value); 
+      outbox.push(floor[0]); 
+    }
+  }
+  return outbox;
+}
+
+
+export default humanResourceMachine;
---
+++ Year_Four/node/doWork_seed.js
@@ -0,0 +1,11 @@
+/**
+ * @param {(string|number)[]} inbox
+ */
+function humanResourceMachine(inbox) {
+  let outbox = [];
+  let floor = [];
+  return outbox;
+}
+
+
+export default humanResourceMachine;
